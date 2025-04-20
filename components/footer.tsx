6d10ff037a26c26b81ac98cb6046f174d891424c
joeb97666 <joeb97666@gmail.com>
2025-04-20 14:46:13 -0700

    update42025-7

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,12 @@ const Footer = () => {
    const currentYear = new Date().getFullYear();
 
     return ( <footer className="border-t">
-        <div className="p-5 text-center inline-flex ">
+        <div className="p-5 text-center flex-col flex-center inline-flex ">
         
             {currentYear} {APP_NAME}. 
 
-<p className="p-5 flex-center">All Rights Reserved.</p>
+
+            <p className="p-5 flex-col text-center"> All Rights Reserved.</p>
         </div>
 
     </footer> );
